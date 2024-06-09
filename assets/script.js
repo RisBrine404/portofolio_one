fadd76a2f79d69c1d6c0e1e1a52fda8572555930
RisBrine <119113183+RisBrine404@users.noreply.github.com>
2024-06-09 22:59:54 +0700

    Update and rename Script.js to script.js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+
+function toggleBackgroundColor() {
+    const body = document.body;
+    if (body.style.backgroundColor === 'white') {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
+}
+public class alert {null, "Hi!, Welcome To MyPortofolio}
+                    {"Sabar ya,Terkadang Foto Butuh Waktu Untuk Ditampilkan"};
