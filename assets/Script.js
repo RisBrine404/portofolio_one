2060723b4daad7ebbf1532758bf49373886be86d
RisBrine <119113183+RisBrine404@users.noreply.github.com>
2024-06-09 22:43:02 +0700

    Add files via upload

---
+++ assets/Script.js
@@ -0,0 +1,25 @@
+
+function toggleBackgroundColor() {
+    const body = document.body;
+    if (body.style.backgroundColor === 'white') {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
+}
+import javax.swing.JOptionPane;
+
+public class AlertExample {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hi!, Welcome To MyPortofolio", "Hello World", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+import javax.swing.JOptionPane;
+
+public class AlertExample {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Sabar ya,Terkadang Foto Butuh Waktu Untuk Ditampilkan", "Informasi", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
