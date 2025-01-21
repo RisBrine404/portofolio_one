7f5b6105c475ef28459a58f291ed7997ed55051c
RisBrine <119113183+RisBrine404@users.noreply.github.com>
2025-01-21 10:54:17 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ function toggleBackgroundColor() {
     if (body.style.backgroundColor === 'white') {
         body.style.backgroundColor = 'black';
         body.style.color = 'white'; 
-        header.style.backgroundColor = '#d8bfd8';
+        header.style.backgroundColor = '#da00cf';
         header.style.color = 'black';
         nav.style.backgroundColor = 'purple';
         nav.style.color = 'white';
